31a8ece227d4ae98ce531494eeaa1905337b2688
deanarriviello <dna18@my.fsu.edu>
2022-11-07 21:10:59 -0500

    created a program to approximate the value of PI based on a user input for the number of terms in the series.

--- src/Pi.java
+++ src/Pi.java
@@ -6,6 +6,27 @@ public class Pi
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int numOfTerms;
+        double pi = 0;
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Compute to how many terms of the series? ");
+        numOfTerms = s.nextInt();
+
+        System.out.println("terms \t PI Approximation");
+
+        for (double i = 1; i <= numOfTerms; i++)
+        {
+            if (i % 2 == 0)
+            {
+                pi -= (4 / ((i * 2) - 1));
+            }
+            else
+            {
+                pi += (4 / ((i * 2) - 1));
+            }
+
+            System.out.printf("%.0f \t" + pi + "\n", i);
+        }
     }
 }
